cf9294ede4b1b869be00cb66f09af9f3e1847346
Callum Fiekert <fiekertc@gmail.com>
2020-12-20 18:30:28 +0000

    Topic 10 - Update Movies Data/Route

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
 import './db';
-import {loadUsers} from './seedData';
+import {loadUsers, loadMovies} from './seedData';
 import usersRouter from './api/users';
 import genresRouter from './api/genres';
 import session from 'express-session';
@@ -27,6 +27,7 @@ const port = process.env.PORT;
 
 if (process.env.SEED_DB) {
   loadUsers();
+  loadMovies();
 }
 
 //session middleware
